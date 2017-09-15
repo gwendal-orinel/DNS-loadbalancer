74f1c9db43a67b47af11f8219f432a94ad436c7c
gwendal-orinel-pro <gwendal.orinel@orange.com>
2017-09-15 13:26:34 +0200

    Update run_CT.sh

--- supermonkey/data/scripts/run_CT.sh
+++ supermonkey/data/scripts/run_CT.sh
@@ -1,5 +1,4 @@
 node=$1
-cloud=$2
 
 echo "Creating container " $node
 curl --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -d @/consul/backups/$node.json -X POST http:/v1.24/containers/create?name=$node
@@ -8,3 +7,5 @@ echo "Starting " $node
 curl --unix-socket /var/run/docker.sock -X POST http:/v1.24/containers/$node/start
 
 echo $node "respawned at "$(date)
+
+echo $(date +%Y-%m-%d' '%H:%M)";create;"$node >> '/consul/supermonkey_history.log';
