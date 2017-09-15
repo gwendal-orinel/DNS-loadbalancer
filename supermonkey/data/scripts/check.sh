793b904d4e59af652a811a0743271119d9c52795
gwendal-orinel-pro <gwendal.orinel@orange.com>
2017-09-15 13:27:39 +0200

    Update check.sh

--- supermonkey/data/scripts/check.sh
+++ supermonkey/data/scripts/check.sh
@@ -1,6 +1,3 @@
-cloud=$1
-node=$2
-
 list=$(curl -s 127.0.0.1:8500/v1/agent/checks | jq -r .[].CheckID)
 
 for node in $list; do
@@ -10,7 +7,7 @@ status=$(curl -s 127.0.0.1:8500/v1/agent/checks | jq -r .$node.Status)
 
 if [ $status != "passing" ]; then
 echo $node " -> Down :" $(date)
-echo $node " -> Down :" $(date) >> /var/log/regenerator.log
+echo $(date +%Y-%m-%d' '%H:%M)";down;" $node >> '/consul/supermonkey_history.log';
 
 bash /consul/scripts/backup_CT.sh $node
 bash /consul/scripts/delete_CT.sh $node
