67d5b4b4739de47f5383e4b1c62561aad0df9146
gwendal-orinel-pro <gwendal.orinel@orange.com>
2017-09-18 13:43:12 +0200

    Update backup_CT.sh

--- supermonkey/data/scripts/backup_CT.sh
+++ supermonkey/data/scripts/backup_CT.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+if [ $1 ]; then
 node=$1
-cloud=$2
 
 echo "Backup config of " $node
 curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X GET http:/v1.24/containers/$node/json > /consul/backups/$node.json
-
-exit 0
+else
+for node in core checker dnspub; do
+echo "Backup config of " $node
+curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X GET http:/v1.24/containers/$node/json > /consul/backups/$node.json
+done
+fi
