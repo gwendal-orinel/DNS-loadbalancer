63392fdc7c4f1acc5a25c1846d1ea71a2f64868a
gwendal-orinel-pro <gwendal.orinel@orange.com>
2017-09-18 13:35:46 +0200

    Update backup_CT.sh

--- supermonkey/data/scripts/backup_CT.sh
+++ supermonkey/data/scripts/backup_CT.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
 node=$1
 cloud=$2
 
 echo "Backup config of " $node
 curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X GET http:/v1.24/containers/$node/json > /consul/backups/$node.json
+
+exit 0
