1f87a229aa12a86572f1d9158a39592fa21c5f66
gwendal-orinel-pro <gwendal.orinel@orange.com>
2017-09-19 10:52:10 +0200

    Update backup_CT.sh

--- supermonkey/data/scripts/backup_CT.sh
+++ supermonkey/data/scripts/backup_CT.sh
@@ -9,10 +9,10 @@ node=$1
 
 #old volume is binded to backup for recovery old data
 echo "Backup config of " $node
-curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X GET http:/v1.24/containers/$node/json | jq -c '. | .HostConfig.Binds = [.Mounts[0].Name+":/consul" ]' > /consul/backups/$node.json
+curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X GET http:/v1.24/containers/$node/json | jq -c '. | if(.Mount != null) then .HostConfig.Binds = [.Mounts[0].Name+":"+.Mounts[0].Destination ] else . end' > /consul/backups/$node.json
 else
 for node in core checker dnspub; do
 echo "Backup config of " $node
-curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X GET http:/v1.24/containers/$node/json | jq -c '. | .HostConfig.Binds = [.Mounts[0].Name+":/consul" ]' > /consul/backups/$node.json
+curl -s --unix-socket /var/run/docker.sock -H "Content-Type: application/json" -X GET http:/v1.24/containers/$node/json | jq -c '. | if(.Mount != null) then .HostConfig.Binds = [.Mounts[0].Name+":"+.Mounts[0].Destination ] else . end' > /consul/backups/$node.json
 done
 fi
