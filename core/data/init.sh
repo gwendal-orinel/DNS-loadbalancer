a79d10eca19bd3c207edc786bab48abd6d4d61e4
gwendal-orinel-pro <gwendal.orinel@orange.com>
2017-09-19 16:00:52 +0200

    Update init.sh

--- core/data/init.sh
+++ core/data/init.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-cat /consul/config2.json | jq ' . | .watches[0].handler = "curl -X PUT -d \""+.domain+"\" \"http://localhost:8500/v1/kv/CONFIG/DNS/domain?token="+.acl_token+"\"" ' > /consul/config.json
+
+#migrate token & domain name into
+cat /consul/config.json | jq ' . | .watches[0].handler = "curl -X PUT -d \""+.domain+"\" \"http://localhost:8500/v1/kv/CONFIG/DNS/domain?token="+.acl_token+"\"" ' > /consul/config2.json
+mv /consul/config2.json /consul/config.json
+
+#start apache service
+/etc/init.d/bind9 start
