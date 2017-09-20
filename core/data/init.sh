e7e03d9364eaeab524487a4e16c0b6dfb6fac62d
gwendal-orinel-pro <gwendal.orinel@orange.com>
2017-09-20 09:35:24 +0200

    Update init.sh

--- core/data/init.sh
+++ core/data/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #create domain key in consul kv
-DOMAINKEY=$(cat /consul/config.json | jq -r ' . | {domain: .domain,token: .acl_token}| "curl -X PUT -d \""+.domain+"\" http://localhost:8500/v1/kv/CONFIG/DNS/domain?token="+.token+"" ')
+DOMAINKEY=$(cat /consul/config.json | jq -r ' . | {domain: .domain,token: .acl_token}| "curl -X PUT -d "+.domain+" http://localhost:8500/v1/kv/CONFIG/DNS/domain?token="+.token+"" ')
 $DOMAINKEY
 
 #start apache service
